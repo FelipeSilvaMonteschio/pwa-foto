103675ea30c41195e59cbcb646c0dae2890c40a0
FelipeSilvaMonteschio <felipe.monteschio@outlook.com>
2023-12-05 13:13:44 -0400

    comit

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ document.querySelector('button').addEventListener('click', () => {
     var context = canvas.getContext('2d');
     context.drawImage(video, 0, 0);
     var link = document.createElement('a');
+    link.download = 'imagem.png';
     link.href = canvas.toDataURL();
     link.textContent = 'Clique para baixar a imagem';
     document.body.appendChild(link);
